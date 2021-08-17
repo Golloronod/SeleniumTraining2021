8629760b3d7bb930bebf1169f9b7419ccddec2d9
Golloronod <golloronod@gmail.com>
2021-08-16 23:32:25 -0400

    4th

--- src/test/java/SecondSeleniumTest.java
+++ src/test/java/SecondSeleniumTest.java
@@ -28,7 +28,7 @@ public class SecondSeleniumTest {
 //        System.out.println(((HasCapabilities) drv1).getCapabilities());
     }
 
-//    @AfterEach
+    @AfterEach
     void closeUp(){
         drv1.close();
     }
@@ -38,37 +38,32 @@ public class SecondSeleniumTest {
         drv1.get("http://158.101.173.161/");
         WebDriverWait wait = new WebDriverWait(drv1, 10);
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".form-control")));
-//        drv1.findElement(By.name("q")).sendKeys("Selenium" + Keys.ENTER);
-//        drv1.findElement(By.cssSelector(".form-control"));
-//        Assertions.assertEquals("Selenium: Benefits, Uses, Side Effects, Dosage, and More", drv1.findElement(By.tagName("h3")).getText());
 
         // 1. Looking for the web element for the "Search Products" field
-        System.out.println(drv1.findElement(By.cssSelector(".form-control")).hashCode());
-//        System.out.println(drv1.findElement(By.cssSelector("[name=query]")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector(".form-control")).getAttribute("placeholder"));
 
         // 2. Looking for the web element for the "Customer Service" field
-        System.out.println(drv1.findElement(By.cssSelector(".customer-service")).hashCode());
-//        System.out.println(drv1.findElement(By.cssSelector("li.customer-service")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("div#default-menu ul.nav.navbar-nav.navbar-right li.customer-service")).getText());
 
         // 3. Looking for the web element for the "Remember Me" checkbox
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".dropdown-toggle")));
         drv1.findElement(By.cssSelector(".fa.fa-user")).click();
-        System.out.println(drv1.findElement(By.cssSelector("[name=remember_me]")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("form.navbar-form div.checkbox input[type=checkbox]")).getAttribute("name"));
 
         // 4. Looking for the web element for the "Lost your password?" link
-        System.out.println(drv1.findElement(By.cssSelector("#default-menu .text-center:last-child")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("ul.dropdown-menu li.text-center:last-child")).getText());
 
         // 5. Looking for the web element for the "$18" price in Campaign Products
-        System.out.println(drv1.findElement(By.cssSelector("#box-campaign-products .price-wrapper .campaign-price")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("#box-campaign-products .price-wrapper .campaign-price")).getText());
 
         // 6. Looking for the web element for the "Red Duck" image in Popular Products
-        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products div.image-wrapper img[alt*='Red Duck']")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products div.image-wrapper img[alt*='Red Duck']")).getAttribute("alt"));
 
         // 7. Looking for the web element for the 4th price in Popular Products
-        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products article.product-column:nth-child(4) div.price-wrapper span.price")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products article.product-column:nth-child(4) div.price-wrapper span.price")).getText());
 
         // 8. Looking for the web element for the "Red Duck" image in Popular Products
-        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products div.sticker.sale[title='On Sale']")).hashCode());
+        System.out.println(drv1.findElement(By.cssSelector("section#box-popular-products div.sticker.sale[title='On Sale']")).getText());
 
     }
 
